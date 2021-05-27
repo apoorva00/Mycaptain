2cae94c98d883a3d4fb6a2fd9c0fa45af4ba80bf
apoorva00 <84899106+apoorva00@users.noreply.github.com>
2021-05-27 13:19:39 +0530

    Add files via upload

---
+++ product.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    double n1, n2, prod;
+    cout << "Enter two numbers: ";
+    cin >>n1>>n2;
+    prod=n1*n2;  
+    cout << "Product of two numbers = " << prod;    
+    return 0;
+}
